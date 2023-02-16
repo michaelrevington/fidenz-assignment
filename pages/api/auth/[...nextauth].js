b7adbf654f87c032057eb747210da5e3aa5fb0af
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-02-16 15:39:27 +0530

    next-auth auth0 provider implementation

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import Auth0Provider from 'next-auth/providers/auth0'
+
+export default NextAuth({
+    providers: [
+        Auth0Provider({
+            issuer: process.env.DOMAIN,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET, 
+            authorization: `${process.env.DOMAIN}/authorize?response_type=code&prompt=login`,
+        })
+    ]
+})
