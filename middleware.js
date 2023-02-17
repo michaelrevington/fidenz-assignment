0ada96c14c98a3cc61332da787df7cc5885a289a
Ravindu Denuwan <ravindudenuwan.dev@gmail.com>
2023-02-17 09:17:17 +0530

    shorter code

--- middleware.js
+++ middleware.js
@@ -3,9 +3,8 @@ import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
     callbacks: {
-        authorized: ({ req, token }) => {
-            if (token) return true;
-            return false;
+        authorized: ({ req, token }) => { 
+            return !!token;
         }
     }
 })
